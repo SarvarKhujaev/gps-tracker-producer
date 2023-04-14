aed8df96d4b3e8fbe40a19c4c02be3e1bb68faf2
Sarvar <@sarvar.khujaev>
2023-04-14 11:51:58 +0500

    first try

--- src/main/java/com/ssd/mvd/netty/BaseObjectDecoder.java
+++ src/main/java/com/ssd/mvd/netty/BaseObjectDecoder.java
@@ -1,26 +1,30 @@
 package com.ssd.mvd.netty;
 
-import com.ssd.mvd.kafka.KafkaDataControl;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufUtil;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.AttributeKey;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.Channel;
+
 import io.netty.util.ReferenceCountUtil;
+import io.netty.util.AttributeKey;
+
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.ByteBuf;
+
+import com.ssd.mvd.kafka.KafkaDataControl;
+import reactor.core.scheduler.Schedulers;
 import reactor.core.publisher.Flux;
 
-import javax.xml.bind.DatatypeConverter;
-import java.net.SocketAddress;
 import java.nio.charset.StandardCharsets;
+import javax.xml.bind.DatatypeConverter;
+
+import java.net.SocketAddress;
 import java.util.Collection;
 
 public abstract class BaseObjectDecoder extends ChannelInboundHandlerAdapter {
-
     private void saveOriginal( String deviceId, Integer port, Object decodedMessage, Object originalMessage ) {
         try {
             if ( decodedMessage instanceof Position ) {
-                Position position = (Position) decodedMessage;
+                final Position position = (Position) decodedMessage;
                 if (originalMessage instanceof ByteBuf) {
                     ByteBuf buf = (ByteBuf) originalMessage;
                     position.set(Position.KEY_ORIGINAL, ByteBufUtil.hexDump(buf, 0, buf.writerIndex()));
@@ -29,11 +33,7 @@ public abstract class BaseObjectDecoder extends ChannelInboundHandlerAdapter {
                 } position.setDeviceId( deviceId );
                 KafkaDataControl.getInstance().writeToKafka( position );
             }
-        } catch ( Exception e ) {
-            System.out.println( "/n/nError in saveOriginal: " );
-            e.printStackTrace();
-        }
-    }
+        } catch ( Exception e ) { e.printStackTrace(); } }
 
     @Override
     public void channelRead( ChannelHandlerContext ctx, Object msg ) {
@@ -46,12 +46,18 @@ public abstract class BaseObjectDecoder extends ChannelInboundHandlerAdapter {
             }
             if ( decodedMessage != null ) {
                 if (decodedMessage instanceof Collection) {
-                    Flux.fromStream( ( (Collection<?>) decodedMessage ).stream() ).subscribe( o -> {
-                        saveOriginal(ctx.channel().attr(AttributeKey.valueOf("imei")).get().toString(), Integer.valueOf(port), o, msg);
-                        ctx.write( o );
-                    } );
+                    Flux.fromStream( ( (Collection<?>) decodedMessage ).stream() )
+                            .parallel( ( (Collection<?>) decodedMessage ).size() )
+                            .runOn( Schedulers.parallel() )
+                            .map( o -> {
+                                this.saveOriginal(ctx.channel().attr(AttributeKey.valueOf("imei")).get().toString(), Integer.valueOf(port), o, msg);
+                                ctx.write( o );
+                                return ctx; } )
+                            .sequential()
+                            .publishOn( Schedulers.single() )
+                            .subscribe();
                 } else {
-                    saveOriginal(ctx.channel().attr(AttributeKey.valueOf("imei")).get().toString(), Integer.valueOf(port), decodedMessage, msg );
+                    this.saveOriginal(ctx.channel().attr(AttributeKey.valueOf("imei")).get().toString(), Integer.valueOf(port), decodedMessage, msg );
                     ctx.write( decodedMessage );
                 }
             }
